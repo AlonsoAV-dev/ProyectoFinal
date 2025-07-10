27c09b3c28335cbf77401f422a4e778217b9c9d0
AlonsoAV-dev <alonsoalmerco.v@gmail.com>
2025-07-10 18:29:40 -0500

    endpoint user

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -3,7 +3,7 @@ import Sequelize from 'sequelize'
 
 const hostname = 'localhost';
 const username = 'postgres';
-const password = 'alonsoAlve';
+const password = 'admin123';
 const database = 'E-commerce';
 const port = 5432;
 const dialect = 'postgres'
